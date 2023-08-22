903419529febc4e69db0c2fa6516c21b1d566612
Muthu <111842489+Muthumuthu007@users.noreply.github.com>
2023-08-22 19:41:13 +0530

    Add files via upload

---
+++ exp 6/seminar.java
@@ -0,0 +1,54 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class seminar {
+
+    /**
+     * Default constructor
+     */
+    public seminar() {
+    }
+
+    /**
+     * 
+     */
+    public int date;
+
+    /**
+     * 
+     */
+    public int time;
+
+    /**
+     * 
+     */
+    public string track;
+
+    /**
+     * 
+     */
+    public string perspective;
+
+    /**
+     * 
+     */
+    public string product;
+
+    /**
+     * 
+     */
+    public void select seminar() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void preserve seat() {
+        // TODO implement here
+    }
+
+}
---
+++ exp 6/user login.java
@@ -0,0 +1,25 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class user login {
+
+    /**
+     * Default constructor
+     */
+    public user login() {
+    }
+
+    /**
+     * 
+     */
+    public string login id;
+
+    /**
+     * 
+     */
+    public string password;
+
+}
