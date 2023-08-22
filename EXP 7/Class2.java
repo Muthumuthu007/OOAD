ec0e95ba241dce8b4d878e1cd0a8d866c3732e1e
Muthu <111842489+Muthumuthu007@users.noreply.github.com>
2023-08-22 19:44:18 +0530

    Add files via upload

---
+++ EXP 7/Class2.java
@@ -0,0 +1,53 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Class2 {
+
+    /**
+     * Default constructor
+     */
+    public Class2() {
+    }
+
+    /**
+     * 
+     */
+    public int credit_card no;
+
+    /**
+     * 
+     */
+    public double amount spent;
+
+    /**
+     * 
+     */
+    public void credit card() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void getcreditamount() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void getamountspend() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void calculate amount() {
+        // TODO implement here
+    }
+
+}
---
+++ EXP 7/payable.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class payable {
+
+    /**
+     * Default constructor
+     */
+    public payable() {
+    }
+
+    /**
+     * 
+     */
+    public void calculation_amount() {
+        // TODO implement here
+    }
+
+}
